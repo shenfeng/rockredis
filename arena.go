4ef2f8e55ec78ce2a75e149dd5cdaac9cbba6b62
feng <shenedu@gmail.com>
2014-08-18 11:21:31 +0800

    more comment; fix wrong offset get set when Arena's constructor get called

--- arena.go
+++ arena.go
@@ -1,14 +1,15 @@
 package main
 
 type Arena struct {
-	ram    []byte
+	ram    []byte // save memory allocation, which is slow: ~80ns per allocation
 	offset int
 }
 
 func NewArena(size int) *Arena {
-	return &Arena{ram: make([]byte, size)}
+	return &Arena{ram: make([]byte, 0)}
 }
 
+// Please make sure all ram allocated from a.ram are free to be reused
 func (a *Arena) Reset() { a.offset = 0 }
 
 func (a *Arena) Allocate(size int) []byte {
@@ -17,10 +18,9 @@ func (a *Arena) Allocate(size int) []byte {
 		a.offset += size
 		return a.ram[off:a.offset]
 	} else if size > len(a.ram) {
-		return make([]byte, size)
-	} else {
-		// gc take care of it
-		a.ram = make([]byte, len(a.ram))
+		return make([]byte, size) //  allocate directly
+	} else { // allocate ram get all used
+		a.ram = make([]byte, len(a.ram)) // gc take care of the old a.ram
 		a.offset = size
 		return a.ram[0:size]
 	}
